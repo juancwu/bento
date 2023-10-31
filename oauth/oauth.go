75e6a2f7a48b692836f23c20f8e7a2c582c8e745
jc <46619361+juancwu@users.noreply.github.com>
2023-10-31 15:30:18 -0400

    add oauth handler

---
+++ oauth/oauth.go
@@ -0,0 +1,32 @@
+package oauth
+
+import (
+    "net/http"
+
+	"github.com/go-chi/chi/v5"
+
+	"github.com/juancwu/bento/store"
+)
+
+type Handler struct {
+    router chi.Router
+    store *store.Store
+}
+
+func New(s *store.Store) *Handler {
+    h := &Handler{}
+
+    h.router = chi.NewRouter()
+
+    h.store = s
+
+    return h
+}
+
+func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    h.router.ServeHTTP(w, r)
+}
+
+func (h *Handler) urlParam(r *http.Request, key string) string {
+    return chi.URLParam(r, key)
+}
