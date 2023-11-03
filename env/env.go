0ceb7b55c750be62542d49c26d800b6c25e49172
jc <46619361+juancwu@users.noreply.github.com>
2023-11-03 13:53:20 -0400

    add new way to handle envs

--- env/env.go
+++ env/env.go
@@ -7,50 +7,45 @@ import (
 	"github.com/joho/godotenv"
 )
 
-type Env struct {
-    SECRET_KEY string
-    BENTO_DB_URL string
-    BENTO_DB_AUTH_TOKEN string
-    BENTO_DB_CONN string
-}
-
-var EnvKey Env
+const (
+	SECRET_KEY                 = "SECRET_KEY"
+	BENTO_DB_URL               = "BENTO_DB_URL"
+	BENTO_DB_AUTH_TOKEN        = "BENTO_DB_AUTH_TOKEN"
+	BENTO_DB_CONN              = "BENTO_DB_CONN"
+	GITHUB_OAUTH_CLIENT_ID     = "GITHUB_OAUTH_CLIENT_ID"
+	GITHUB_OAUTH_CLIENT_SECRET = "GITHUB_OAUTH_CLIENT_SECRET"
+)
 
 func Load() error {
-    err := godotenv.Load()
-    if err != nil {
-        return err
-    }
+	err := godotenv.Load()
+	if err != nil {
+		return err
+	}
 
-    EnvKey = Env{
-        SECRET_KEY: "SECRET_KEY",
-        BENTO_DB_URL: "BENTO_DB_URL",
-        BENTO_DB_AUTH_TOKEN: "BENTO_DB_AUTH_TOKEN",
-        BENTO_DB_CONN: "BENTO_DB_CONN",
-    }
+	err = checkEnv()
+	if err != nil {
+		return err
+	}
 
-    err = checkEnv()
-    if err != nil {
-        return err
-    }
-
-    return nil
+	return nil
 }
 
 func checkEnv() error {
-    envList := []string{
-        "SECRET_KEY",
-        "BENTO_DB_URL",
-        "BENTO_DB_AUTH_TOKEN",
-        "BENTO_DB_CONN",
-    }
+	envList := []string{
+		SECRET_KEY,
+		BENTO_DB_URL,
+		BENTO_DB_AUTH_TOKEN,
+		BENTO_DB_CONN,
+		GITHUB_OAUTH_CLIENT_ID,
+		GITHUB_OAUTH_CLIENT_SECRET,
+	}
 
-    for _, name := range envList {
-        value := os.Getenv(name)
-        if len(value) == 0 {
-            return fmt.Errorf("Missing mandatory env variable %s", name)
-        }
-    }
+	for _, name := range envList {
+		value := os.Getenv(name)
+		if len(value) == 0 {
+			return fmt.Errorf("Missing mandatory env variable %s", name)
+		}
+	}
 
-    return nil
+	return nil
 }
