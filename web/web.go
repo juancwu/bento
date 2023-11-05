5267cb41b95f24299c9e60dfe12bbd037218e37c
jc <46619361+juancwu@users.noreply.github.com>
2023-11-05 17:49:50 -0500

    add method to response with json

--- web/web.go
+++ web/web.go
@@ -1,8 +1,25 @@
 package web
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func Reply(w http.ResponseWriter, message string, code int) {
     w.WriteHeader(code)
     w.Write([]byte(message))
+}
+
+func Json(w http.ResponseWriter, data interface{}, code int) {
+    w.Header().Set("Content-Type", "application/json")
+
+    jsonData, err := json.Marshal(data)
+    if err != nil {
+        w.WriteHeader(http.StatusInternalServerError)
+        w.Write([]byte(`{"error": "Something went wrong marshalling the response object"}`))
+        return
+    }
+
+    w.WriteHeader(code)
+    w.Write(jsonData)
 }
